6da80d5a83919d963466599a6737941fc126b52a
Daniel Kendell <daniel.kendell@mendeley.com>
2014-04-21 23:40:33 +0100

    added "next" command

--- src/mpdui_command.erl
+++ src/mpdui_command.erl
@@ -14,11 +14,16 @@ execute( C, <<"search">>, [ Type, What ] ) ->
 		{ <<"results">>, ResultsStruct }
 	] };
 
+%% Playback
+
+execute( C, <<"next">>, [] ) ->
+	ok( erlmpd:next( C ) );
+
+
 %% Current Playlist
 
 execute( C, <<"add">>, [ Uri ] ) ->
-	ok = erlmpd:add( C, binary_to_list( Uri ) ),
-	{ struct, [ { <<"ok">>, true } ] };
+	ok( erlmpd:add( C, binary_to_list( Uri ) ) );
 execute( C, <<"addid">>, [ Uri ] ) ->
 	SongId = erlmpd:addid( C, Uri ),
 	{ struct, [
@@ -30,8 +35,7 @@ execute( C, <<"addid">>, [ Uri, Position ] ) ->
 		{ <<"id">>, SongId }
 	] };
 execute( C, <<"clear">>, [] ) ->
-	ok = erlmpd:clear( C ),
-	{ struct, [ { <<"ok">>, true } ] };
+	ok( erlmpd:clear( C ) );
 execute( C, <<"playlist">>, [] ) ->
 	Results = erlmpd:playlist( C ),
 
@@ -58,3 +62,6 @@ execute( _Connection, Command, Args ) ->
 		{ <<"cmd">>, Command },
 		{ <<"args">>, Args }
 	] }.
+
+ok( ok ) -> { struct, [ { <<"ok">>, true } ] };
+ok( _ ) -> { struct, [ { <<"ok">>, false } ] }.
