dee1951dbb09e1c3ee3c34e8c933ff45a8d1b519
Daniel Kendell <daniel.kendell@mendeley.com>
2014-04-29 18:55:30 +0100

    removed app wrapper

--- src/mpdui.erl
+++ src/mpdui.erl
@@ -5,4 +5,14 @@
 start() ->
 	lists:map( fun( App ) ->
 		application:start( App )
-	end, [ sasl, ranch, cowlib, crypto, cowboy, mpdui ] ).
+	end, [ sasl, ranch, cowlib, crypto, cowboy ] ),
+
+	Dispatch = cowboy_router:compile([
+		{ '_', [
+			{ "/", cowboy_static, { file, "./priv/index.html" } },
+			{ "/websocket", mpdui_websocket, [] },
+			{ "/static/[...]", cowboy_static, { dir, "./priv/static" } }
+		] }
+	] ),
+
+	cowboy:start_http( http, 100, [ { port, 45001 } ], [ { env, [ { dispatch, Dispatch } ]  } ] ).
