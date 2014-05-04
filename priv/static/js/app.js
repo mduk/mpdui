7ab9a10d728c3d41dbdcc27929f8d963f2d24321
Daniel Kendell <daniel.kendell@mendeley.com>
2014-05-04 03:35:19 +0100

    a better place for that

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1,21 +1,22 @@
 define( function( require ) {
 	var jquery = require( 'jquery' );
-	
-	require( 'bootstrap' );
-
-	require( 'js/ui/nav' ).attachTo( 'nav ul.nav' );
-	require( 'js/ui/mini-control' ).attachTo( '#mini-control-container' );
-	require( 'js/ui/now-playing' ).attachTo( '#now-playing .jumbotron' );
-	require( 'js/ui/queue' ).attachTo( '#queue-container' );
-	require( 'js/ui/search' ).attachTo( '#search-container' );
-	require( 'js/ui/artists' ).attachTo( '#artists-container' );
-	require( 'js/ui/albums' ).attachTo( '#albums-container' );
-	require( 'js/ui/outputs' ).attachTo( '#settings-outputs-container' );
-
-	require( 'js/data/search' ).attachTo( document );
-	require( 'js/data/wsmpd' ).attachTo( document );
 
 	return function() {
+
+		require( 'bootstrap' );
+
+		require( 'js/ui/nav' ).attachTo( 'nav ul.nav' );
+		require( 'js/ui/mini-control' ).attachTo( '#mini-control-container' );
+		require( 'js/ui/now-playing' ).attachTo( '#now-playing .jumbotron' );
+		require( 'js/ui/queue' ).attachTo( '#queue-container' );
+		require( 'js/ui/search' ).attachTo( '#search-container' );
+		require( 'js/ui/artists' ).attachTo( '#artists-container' );
+		require( 'js/ui/albums' ).attachTo( '#albums-container' );
+		require( 'js/ui/outputs' ).attachTo( '#settings-outputs-container' );
+
+		require( 'js/data/search' ).attachTo( document );
+		require( 'js/data/wsmpd' ).attachTo( document );
+
 		jquery('button').tooltip();
 	};
 } );
