645e64dba93696b765a39326b647ed49f4ecc556
Daniel Kendell <daniel@kendell.org>
2014-10-08 21:04:38 +0100

    Oh that's better! Everything seems fine, 'course I could prove that if I'd unit tested it. Ah well. Livin' on the edge! :D

--- priv/static/js/data/mpd.js
+++ priv/static/js/data/mpd.js
@@ -1,4 +1,6 @@
 define( function( require ) {
+	'use strict';
+
 	var defineComponent = require('flight/component'),
 	    wsmpd = require('wsmpd');
 
@@ -9,81 +11,97 @@ define( function( require ) {
 	function mpd() {
 
 		this.after( 'initialize', function() {
-			this.on( document, 'request-search', function( e, d ) {
-				wsmpd.search( d.type, d.what );
-				this.trigger( document, 'change-tab', { to: 'search' } );
-			} );
-
-			this.on( document, 'request-playlistinfo', function() {
-				wsmpd.playlistinfo();
-			} );
-
-			this.on( document, 'request-clear', function() {
-				wsmpd.clear();
-				wsmpd.playlistinfo();
-			} );
-
-			this.on( document, 'request-findadd', function( e, d ) {
-				wsmpd.findadd( d.type, d.what );
-				wsmpd.playlistinfo();
-			} );
-
-			this.on( document, 'request-list', function( e, d ) {
-				wsmpd.list( d.type );
-			} );
-
-			this.on( document, 'request-previous', function( e, d ) {
-				wsmpd.previous();
-			} );
-
-			this.on( document, 'request-next', function( e, d ) {
-				wsmpd.next();
-			} );
-
-			this.on( document, 'request-outputs', function( e, d ) {
-				wsmpd.outputs();
-			} );
-
-			this.on( document, 'request-play', function( e, d ) {
-				wsmpd.play( d.pos );
-			} );
-
-			this.on( document, 'request-delete', function( e, d ) {
-				wsmpd.delete( d.pos );
-				wsmpd.playlistinfo();
-			} );
-
-			this.on( document, 'request-random', function( e, d ) {
-				wsmpd.random( d.state );
-			} );
-
-			this.on( document, 'request-repeat', function( e, d ) {
-				wsmpd.repeat( d.state );
-			} );
-
-			this.on( document, 'request-pause', function( e, d ) {
-				wsmpd.pause( d.state );
-			} );
-
-			this.on( document, 'request-addid', function( e, d ) {
-				wsmpd.addid( d.id );
-				wsmpd.playlistinfo();
-			} );
-
-			this.on( document, 'request-consume', function( e, d ) {
-				wsmpd.consume( d.state );
-			} );
-
-			// Connect to an MPD host
-			this.on( document, 'request-connect', function( e, d ) {
-				wsmpd.connect( d.host );
-			} );
-
-			// Request the current playlistinfo on connection
-			this.on( document, 'websocket-connect', function() {
-				wsmpd.playlistinfo();
-			} );
+			this.on( document, 'request-search', this.onRequestSearch );
+			this.on( document, 'request-playlistinfo', this.onRequestPlaylistinfo );
+			this.on( document, 'request-clear', this.onRequestClear );
+			this.on( document, 'request-findadd', this.onRequestFindadd );
+			this.on( document, 'request-list', this.onRequestList );
+			this.on( document, 'request-previous', this.onRequestPrevious );
+			this.on( document, 'request-next', this.onRequestNext );
+			this.on( document, 'request-outputs', this.onRequestOutputs );
+			this.on( document, 'request-play', this.onRequestPlay );
+			this.on( document, 'request-delete', this.onRequestDelete );
+			this.on( document, 'request-random', this.onRequestRandom );
+			this.on( document, 'request-repeat', this.onRequestRepeat );
+			this.on( document, 'request-pause', this.onRequestPause );
+			this.on( document, 'request-addid', this.onRequestAddid );
+			this.on( document, 'request-consume', this.onRequestConsume );
+			this.on( document, 'request-connect', this.onRequestConnect );
+			this.on( document, 'websocket-connect', this.onWebsocketConnect );
 		} );
+
+		this.onRequestSearch = function( e, d ) {
+			wsmpd.search( d.type, d.what );
+			this.trigger( document, 'change-tab', { to: 'search' } );
+		};
+
+		this.onRequestPlaylistinfo = function( e, d ) {
+			wsmpd.playlistinfo();
+		};
+
+		this.onRequestClear = function( e, d ) {
+			wsmpd.clear();
+			wsmpd.playlistinfo();
+		};
+
+		this.onRequestFindadd = function( e, d ) {
+			wsmpd.findadd( d.type, d.what );
+			wsmpd.playlistinfo();
+		};
+
+		this.onRequestList = function( e, d ) {
+			wsmpd.list( d.type );
+		};
+
+		this.onRequestPrevious = function( e, d ) {
+			wsmpd.previous();
+		};
+
+		this.onRequestNext = function( e, d ) {
+			wsmpd.next();
+		};
+
+		this.onRequestOutputs = function( e, d ) {
+			wsmpd.outputs();
+		};
+
+		this.onRequestPlay = function( e, d ) {
+			wsmpd.play( d.pos );
+		};
+
+		this.onRequestDelete = function( e, d ) {
+			wsmpd.delete( d.pos );
+			wsmpd.playlistinfo();
+		};
+
+		this.onRequestRandom = function( e, d ) {
+			wsmpd.random( d.state );
+		};
+
+		this.onRequestRepeat = function( e, d ) {
+			wsmpd.repeat( d.state );
+		};
+
+		this.onRequestPause = function( e, d ) {
+			wsmpd.pause( d.state );
+		};
+
+		this.onRequestAddid = function( e, d ) {
+			wsmpd.addid( d.id );
+			wsmpd.playlistinfo();
+		};
+
+		this.onRequestConsume = function( e, d ) {
+			wsmpd.consume( d.state );
+		};
+
+		this.onRequestConnect = function( e, d ) {
+			wsmpd.connect( d.host );
+		};
+
+		this.onWebsocketConnect = function() {
+			wsmpd.playlistinfo();
+		};
 
 	}
 
